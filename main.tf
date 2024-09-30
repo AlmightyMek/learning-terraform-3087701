4833c46b960ce4b16cd4a44e709c327040ce3de7
Emeka Nwangwu <55567776+AlmightyMek@users.noreply.github.com>
2024-09-29 21:22:45 -0500

    typo lol

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_securtiy_groups_ids = [aws_security_group.blog.id]
+  vpc_security_groups_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
